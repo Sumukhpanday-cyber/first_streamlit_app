05cdd9938e7a076f46068588dd4188413e1e5b14
Sumukhpanday-cyber <113578632+Sumukhpanday-cyber@users.noreply.github.com>
2023-03-03 06:20:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.text("chicken");
 streamlit.text("manchurian");
 streamlit.text("eating food ");
 import pandas;
-myfruitlist=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+myfruitlist=pandas.read_csv("uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 streamlit.dataframe("myfruitlist");
