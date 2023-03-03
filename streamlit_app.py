ea4c394a8f9fe8694627cc24e1890e477e9ba643
Sumukhpanday-cyber <113578632+Sumukhpanday-cyber@users.noreply.github.com>
2023-03-03 06:14:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit;
 streamlit.title("My parent new healthy dinner");
+streamlit.header("Breakfast menu");
+streamlit.text("chicken");
+streamlit.text("manchurian");
+streamlit.text("eating food ");
