a0d21fdc3238cfa12cbfdf14be9849a4e4d8cf7c
Sumukhpanday-cyber <113578632+Sumukhpanday-cyber@users.noreply.github.com>
2023-03-07 16:02:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,5 @@
 import streamlit;
-streamlit.title('My parents new healthy dinner');
-streamlit.header("Breakfast menu");
-streamlit.text("chicken");
-streamlit.text("manchurian");
-streamlit.text("eating food ");
-import pandas;
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-fruit_selected=streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruit_selected];
-streamlit.dataframe(fruits_to_show);
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.title('My parents new healthy diner');
+
 
 
