d0bad8fc6519aad472ff77b535f88ca8d136b32a
Sumukhpanday-cyber <113578632+Sumukhpanday-cyber@users.noreply.github.com>
2023-03-08 18:21:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,16 +44,5 @@ except URLError as e :
     
   
   
-streamlit.header("The fruit list contains ")
-streamlit.dataframe(my_data_rows)	
-streamlit.header('Fruityvice fruit Advice')
-try:
-  fruit_choice=streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-       streamlit.error("please select a fruit to get information")
-  else:
-       back_from_function=get_fruityvice_data(fruit_choice)
-       streamlit.dataframe(back_from_function)
-except URLError as e :
 
  
