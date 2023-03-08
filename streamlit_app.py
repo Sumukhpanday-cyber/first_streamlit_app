849bb223c12244c5222f60b13df3279e77a859aa
Sumukhpanday-cyber <113578632+Sumukhpanday-cyber@users.noreply.github.com>
2023-03-08 17:12:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,18 @@ my_cur.execute("insert  into fruit_load_list values('from  streamlit')")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit list contains ")
 streamlit.dataframe(my_data_rows)	
+streamlit.header('Fruityvice fruit Advice')
+try:
+  fruit_choice=streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("please select a fruit to get information")
+  else:
+      fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e :
+  streamlit.error()
+      
 
 
 
