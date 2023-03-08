3b4937abac066956efa8d39be212b62e7d94b876
Sumukhpanday-cyber <113578632+Sumukhpanday-cyber@users.noreply.github.com>
2023-03-08 18:51:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,27 @@
-import streamlit;
-streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast Favourites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale,Spinach & Rocket Smoothiee')
-streamlit.text('Hard Boiled Free Range Egg ')
-streamlit.text('Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import streamlit
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
-
-streamlit.dataframe(fruits_to_show)
+import requests
 import snowflake.connector
 from urllib.error import URLError
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("insert  into fruit_load_list values('from  streamlit')")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit list contains ")
-streamlit.dataframe(my_data_rows)	
+
+
+streamlit.title('My Parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🧇Omega 3 and Blueberry Oatmeal')
+streamlit.text('🍣Kale, spinach and rocket smoothie')
+streamlit.text('🍱Hard-Boiled Free-Range Egg')
+streamlit.text('🍗Avocado Toast')
+
+streamlit.header('🍯🍡Build Your Own Fruit Smoothie🍙🍤')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
@@ -41,7 +38,10 @@ try:
         streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
-    # streamlit.text(fruityvice_response.json())
+
+
+
+# streamlit.text(fruityvice_response.json())
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
 
@@ -72,8 +72,6 @@ if streamlit.button('Add a Fruit to the List'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
 streamlit.stop()
-    
-
 
 
     
